74d1eaf9e28bc3448a64efcf5948e493a6f91849
IronProg <rnberny1@gmail.com>
2025-10-11 10:43:36 -0300

    embed youtube video

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,10 +7,17 @@ const Home = () => {
         <section className="hero">
           <h2>Bem-vindo ao Futuro da Agricultura</h2>
           <p>Monitore suas plantações com inteligência e aumente sua produtividade.</p>
+
+          <iframe
+            src="https://www.youtube.com/embed/0-HGp8A-6DM?rel=0"
+            frameborder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; modestbranding; showinfo=0; fullscreen"
+          >
+          </iframe>
+
           <NavLink to={'/funcionalidades'} className={'btn'}>
             Saiba mais
           </NavLink>
-          <a href="https://youtu.be/sz-3YSf6oNo" target="_blank"><p> Link para o Pitch</p></a>
         </section>
 
         <section className="destaques">
